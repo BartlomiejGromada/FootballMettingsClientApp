92160158fb91123b82539d26be679c4ad487699f
Bartłomiej Gromada <bartkuuu8167@gmail.com>
2023-02-10 20:20:28 +0100

    Init project and definde settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-uses-react": OFF,
+    "react/react-in-jsx-scope": OFF,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { DrawerStyled } from "@components/ui/DrawerStyled";
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <p>Test</p>
+      <DrawerStyled />
+    </Box>
+  );
+}
+
+export default App;
